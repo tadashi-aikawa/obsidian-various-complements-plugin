935d7de1633b9a7685349a97302a0e908e10b215
yukang <moorekang@gmail.com>
2021-11-26 19:57:18 +0800

    fix issue #30, take care of uppercase

--- src/util/strings.ts
+++ src/util/strings.ts
@@ -2,14 +2,23 @@
  * This function uses case-sensitive logic if a second argument has an upper case. Otherwise, uses case-insensitive logic.
  */
 export function caseIncludes(one: string, other: string): boolean {
-  const lowerOther = other.toLowerCase();
-  return lowerOther === other
-    ? one.toLowerCase().includes(lowerOther)
-    : one.includes(other);
+    return one.includes(other);
 }
 
 export function caseIncludesWithoutSpace(one: string, other: string): boolean {
   return caseIncludes(one.replace(/ /g, ""), other.replace(/ /g, ""));
+}
+
+export function capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function upperCaseIncludesWithoutSpace(one: string, other: string): boolean {
+  if (capitalizeFirstLetter(other) != other) {
+    return false;
+  }
+  let capitalizedOne = capitalizeFirstLetter(one);
+  return caseIncludesWithoutSpace(capitalizedOne, other);
 }
 
 export function lowerStartsWith(a: string, b: string): boolean {
