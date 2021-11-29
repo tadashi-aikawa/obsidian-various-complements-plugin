18463293e4dc21457f0d8d75050d702d41ce765b
tadashi-aikawa <syou.maman@gmail.com>
2021-11-29 21:15:26 +0900

    Add `\t` to trim pattern

--- src/tokenizer/tokenizers/DefaultTokenizer.ts
+++ src/tokenizer/tokenizers/DefaultTokenizer.ts
@@ -4,7 +4,7 @@ function pickTokens(content: string, trimPattern: RegExp): string[] {
   return content.split(trimPattern).filter((x) => x !== "");
 }
 
-export const TRIM_CHAR_PATTERN = /[\n\\\[\]/()<>"'.,|;:*~ `]/g;
+export const TRIM_CHAR_PATTERN = /[\n\t\\\[\]/()<>"'.,|;:*~ `]/g;
 export class DefaultTokenizer implements Tokenizer {
   tokenize(content: string): string[] {
     return pickTokens(content, this.getTrimPattern());
