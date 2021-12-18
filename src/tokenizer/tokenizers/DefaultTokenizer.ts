35d4a2eff7717156de2666b5d5ae985b5ea8a7fb
tadashi-aikawa <syou.maman@gmail.com>
2021-12-18 13:05:23 +0900

    Improve tokenize about symbols on default strategy (#50)

--- src/tokenizer/tokenizers/DefaultTokenizer.ts
+++ src/tokenizer/tokenizers/DefaultTokenizer.ts
@@ -4,7 +4,7 @@ function pickTokens(content: string, trimPattern: RegExp): string[] {
   return content.split(trimPattern).filter((x) => x !== "");
 }
 
-export const TRIM_CHAR_PATTERN = /[\n\t\\\[\]/:()<>"'.,|;*~ `]/g;
+export const TRIM_CHAR_PATTERN = /[\n\t\\\[\]/:?!=()<>"'.,|;*~ `]/g;
 export class DefaultTokenizer implements Tokenizer {
   tokenize(content: string, raw?: boolean): string[] {
     return pickTokens(content, raw ? / /g : this.getTrimPattern());
