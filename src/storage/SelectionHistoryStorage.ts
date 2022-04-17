06abbedc46d1e43f94a4464a7bf3ce9e9818a340
tadashi-aikawa <syou.maman@gmail.com>
2022-04-18 02:09:34 +0900

    Improve logic by considering the last selected time (#114)

--- src/storage/SelectionHistoryStorage.ts
+++ src/storage/SelectionHistoryStorage.ts
@@ -2,16 +2,43 @@ import type { Word } from "../model/Word";
 import type { PartialRequired } from "../types";
 
 export type HitWord = PartialRequired<Word, "hit" | "completionDistance">;
+export type SelectionHistory = {
+  count: number;
+  lastUpdated: number;
+};
 
 export type SelectionHistoryByValue = {
-  [value: string]: {
-    accumulatedCompletionDistance: number;
-    lastUpdated: number;
-  };
+  [value: string]: SelectionHistory;
 };
 
 function word2Key(word: HitWord): string {
   return `${word.hit}/${word.type}`;
+}
+
+const SEC = 1000;
+const MIN = SEC * 60;
+const HOUR = MIN * 60;
+const DAY = HOUR * 24;
+const WEEK = DAY * 7;
+
+function calcScore(history: SelectionHistory | undefined): number {
+  if (!history) {
+    return 0;
+  }
+
+  const behind = Date.now() - history.lastUpdated;
+
+  if (behind < MIN) {
+    return 8 * history.count;
+  } else if (behind < HOUR) {
+    return 4 * history.count;
+  } else if (behind < DAY) {
+    return 2 * history.count;
+  } else if (behind < WEEK) {
+    return 0.5 * history.count;
+  } else {
+    return 0.25 * history.count;
+  }
 }
 
 export class SelectionHistoryStorage {
@@ -24,31 +51,27 @@ export class SelectionHistoryStorage {
   increment(word: HitWord): void {
     const key = word2Key(word);
 
-    const distance = word.completionDistance;
     if (this.data[key]) {
       this.data[key] = {
-        accumulatedCompletionDistance:
-          this.data[key].accumulatedCompletionDistance + distance + 1,
+        count: this.data[key].count + 1,
         lastUpdated: Date.now(),
       };
     } else {
       this.data[key] = {
-        accumulatedCompletionDistance: distance + 1,
+        count: 1,
         lastUpdated: Date.now(),
       };
     }
   }
 
   compare(w1: HitWord, w2: HitWord): -1 | 0 | 1 {
-    const distance1 =
-      this.data[word2Key(w1)]?.accumulatedCompletionDistance ?? 0;
-    const distance2 =
-      this.data[word2Key(w2)]?.accumulatedCompletionDistance ?? 0;
+    const score1 = calcScore(this.data[word2Key(w1)]);
+    const score2 = calcScore(this.data[word2Key(w2)]);
 
-    if (w1.hit.length - distance1 === w2.hit.length - distance2) {
+    if (score1 === score2) {
       return 0;
     }
 
-    return w1.hit.length - distance1 > w2.hit.length - distance2 ? 1 : -1;
+    return score1 > score2 ? -1 : 1;
   }
 }
