bddfd19239453cf9cf39879258973c1bc8391c8a
tadashi-aikawa <syou.maman@gmail.com>
2025-08-23 19:16:04 +0900

    fix: Partial matching trims leading whitespace when inserting a link in front of certain punctuation #358

--- src/tokenizer/tokenizers/DefaultTokenizer.test.ts
+++ src/tokenizer/tokenizers/DefaultTokenizer.test.ts
@@ -67,8 +67,8 @@ describe.each<{
   ${{}}                                    | ${"aa:bb:cc"}      | ${[{ word: "aa:bb:cc", offset: 0 }, { word: "bb:cc", offset: 3 }, { word: "cc", offset: 6 }]}
   ${{}}                                    | ${"## @smi"}       | ${[{ word: "## @smi", offset: 0 }, { word: "@smi", offset: 3 }]}
   ${{}}                                    | ${"$\\alpha"}      | ${[{ word: "$\\alpha", offset: 0 }, { word: "\\alpha", offset: 1 }]}
-  ${{}}                                    | ${"::one::two"}    | ${[{ word: "::one::two", offset: 0 }, { word: ":one::two", offset: 1 }, { word: "one::two", offset: 2 }, { word: ":two", offset: 6 }, { word: "two", offset: 7 }]}
-  ${{}}                                    | ${"__a __c__ e__"} | ${[{ word: "__a __c__ e__", offset: 0 }, { word: "_a __c__ e__", offset: 1 }, { word: "a __c__ e__", offset: 2 }, { word: "__c__ e__", offset: 4 }, { word: "_c__ e__", offset: 5 }, { word: "c__ e__", offset: 6 }, { word: "_ e__", offset: 8 }, { word: " e__", offset: 9 }, { word: "e__", offset: 10 }, { word: "_", offset: 12 }, { word: "", offset: 13 }]}
+  ${{}}                                    | ${"::one::two"}    | ${[{ word: "::one::two", offset: 0 }, { word: "one::two", offset: 2 }, { word: "two", offset: 7 }]}
+  ${{}}                                    | ${"__a __c__ e__"} | ${[{ word: "__a __c__ e__", offset: 0 }, { word: "a __c__ e__", offset: 2 }, { word: "c__ e__", offset: 6 }, { word: "e__", offset: 10 }]}
   ${{ treatUnderscoreAsPartOfWord: true }} | ${"__a __c__ e__"} | ${[{ word: "__a __c__ e__", offset: 0 }, { word: "__c__ e__", offset: 4 }, { word: "e__", offset: 10 }]}
 `("recursiveTokenize", ({ constructorArgs, content, expected }) => {
   test(`recursiveTokenize(${content}) = ${expected}`, () => {
--- src/tokenizer/tokenizers/DefaultTokenizer.ts
+++ src/tokenizer/tokenizers/DefaultTokenizer.ts
@@ -1,32 +1,65 @@
-import { splitRaw } from "../../util/strings";
+import type { TrimTarget } from "../tokenizer";
 import { AbstractTokenizer } from "./AbstractTokenizer";
 
-function pickTokens(content: string, trimPattern: RegExp): string[] {
-  return content.split(trimPattern).filter((x) => x !== "");
-}
+type PreviousType = "none" | "trim" | "others";
 
 export class DefaultTokenizer extends AbstractTokenizer {
   tokenize(content: string, raw?: boolean): string[] {
-    const tokens = raw
-      ? Array.from(splitRaw(content, this.getTrimPattern("indexing"))).filter(
-          (x) => x !== " ",
-        )
-      : pickTokens(content, this.getTrimPattern("indexing"));
-    return tokens.map((x) => x.replace(/\.+$/g, ""));
+    const tokenized = Array.from(this.__tokenize(content, "indexing"));
+    return raw
+      ? tokenized.map((x) => x.word)
+      : tokenized
+          .map((x) => x.word)
+          .filter((x) => !x.match(this.getTrimPattern("indexing")))
+          .map((x) => x.replace(/\.+$/g, ""));
   }
 
   recursiveTokenize(content: string): { word: string; offset: number }[] {
-    const trimIndexes = Array.from(
-      content.matchAll(this.getTrimPattern("input")),
-    )
-      .sort((a, b) => a.index! - b.index!)
-      .map((x) => x.index!);
-    return [
-      { word: content, offset: 0 },
-      ...trimIndexes.map((i) => ({
-        word: content.slice(i + 1),
-        offset: i + 1,
-      })),
-    ];
+    const offsets = Array.from(this.__tokenize(content, "input"))
+      .filter((x) => !x.word.match(this.getTrimPattern("input")))
+      .map((x) => x.offset);
+
+    const results = offsets.map((i) => ({
+      word: content.slice(i),
+      offset: i,
+    }));
+    return results[0].offset === 0
+      ? results
+      : [{ word: content, offset: 0 }, ...results];
+  }
+
+  // Diffirent with _tokenize of other tokenizers
+  private *__tokenize(
+    content: string,
+    target: TrimTarget,
+  ): Iterable<{ word: string; offset: number }> {
+    let startIndex = 0;
+    let previousType: PreviousType = "none";
+
+    for (let i = 0; i < content.length; i++) {
+      if (content[i].match(super.getTrimPattern(target))) {
+        const word = content.slice(startIndex, i);
+        if (word !== "") {
+          yield { word, offset: startIndex };
+        }
+        previousType = "trim";
+        startIndex = i;
+        continue;
+      }
+
+      if (previousType === "others" || previousType === "none") {
+        previousType = "others";
+        continue;
+      }
+
+      yield { word: content.slice(startIndex, i), offset: startIndex };
+      previousType = "others";
+      startIndex = i;
+    }
+
+    yield {
+      word: content.slice(startIndex, content.length),
+      offset: startIndex,
+    };
   }
 }
